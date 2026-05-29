ce9368e8eeac38181ea46636526280a4d219a518
bhavishek01 <bhavisheklama01@gmail.com>
2026-05-29 18:58:17 +0545

    cancle button added

--- src/php/get_upcoming_events.php
+++ src/php/get_upcoming_events.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id'])) exit('Unauthorized');
 require_once '../database/db_con.php';
 $result = $conn->query("SELECT * FROM events WHERE date >= CURDATE() ORDER BY date ASC LIMIT 20");
 echo json_encode($result->fetch_all(MYSQLI_ASSOC));
